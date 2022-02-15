57e20d47067f5712da047ed570d48d93e8f1dd75
Matheus de Araújo Alves <matheus.aalves@MacBook-Air-de-Matheus.local>
2022-02-15 13:53:40 -0300

    Requisito 3 Grupo 13

---
+++ src/pages/Options.js
@@ -0,0 +1,15 @@
+// Esta é a tela de configurações do requisito 3.
+import React from 'react';
+
+class ConfigScreen extends React.Component {
+  render() {
+    return (
+      <h1 data-testid="settings-title">
+        Configurações
+      </h1>
+
+    );
+  }
+}
+
+export default ConfigScreen;
